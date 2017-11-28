39a8fcbd60f123a354594c32e7b81e2d6b170355
NadiaKhatib <nadosh180@hotmail.com>
2017-11-28 13:46:21 +0200

    generic handler

---
+++ src/handler.js
@@ -0,0 +1,29 @@
+const fs=require('fs');
+const path=require('path');
+const generic =(request,response)=>{
+  const endpoint=request.url;
+  const extension=endpoint.split('.')[1];
+  const fileType={
+    html:'text/html',
+    css:'text/css',
+    js:'application/javascript',
+    ico:'image/x-icon',
+    json:'application/json'
+  };
+    fs.readFile(path.join(__dirname,'..',endpoint),(error,file)=>{
+
+      if(error){
+        response.writeHead(500,'content-Type:text/html');
+        response.end('<h1> Internal server Error </h1>');
+      }
+      else{
+
+        response.writeHead(200,'content-Type:'+fileType[extension]);
+        response.end(file);
+      }
+});
+
+};
+module.exports={
+  generic
+};
